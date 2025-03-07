336d1ccb8b603c102f6d8f404c1d228c303c36f1
hthnhpzy <beanhateyou@gmail.com>
2025-03-07 23:35:25 +0700

    Update and rename script.jss to script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u = username.value
   let p = password.value
-  if (u=="bao" && p=="123")
+  if (u=="vy" && p=="123")
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block"
